6d10de81b95f6ff67c9b7606c0e7dd5a62a5b5ab
louisql <102247814+louisql@users.noreply.github.com>
2022-12-27 22:21:27 -0500

    Badic header implemented

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,27 @@
 import { Fragment } from "react";
 
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
+/* Using import mealsImage to practice  */
+
 const Header = (props) => {
-  return
-  /* 
+  return (
+    /* 
     Reminder - Fragment used to avoid having too many div in the code. Just a container - could have also imported
     React only and do react.fragment instead of Fragment
   */
-  <Fragment>
-    <header></header>
-    <div></div>
-  </Fragment>;
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Ordering App</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        {/* because of the dash in main-image we can't use the dot notation */}
+        <img src={mealsImage} alt="A table full of food" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
