5591711ef8aca6305bcda118003f5912b2201bdd
louisql <102247814+louisql@users.noreply.github.com>
2022-12-29 18:39:25 -0500

    Introduction of context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -17,7 +18,7 @@ function App() {
 
   return (
     /* <> </> replacing Fragment */
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       {/* {cartIsShown && <Cart />} expression to display if both are true (Cart will always be true - it is a shortcut notation) */}
 
@@ -27,8 +28,8 @@ function App() {
       <main>
         <Meals />
       </main>
-    </>
-  );
+    </CartProvider>
+  )
 }
 
 export default App;
