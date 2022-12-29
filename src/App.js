547bd3b48fc5775e524e5a6f26eabad671241f8b
louisql <102247814+louisql@users.noreply.github.com>
2022-12-29 17:44:07 -0500

    Advancement work on Modal - open/Close functions added through props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () =>{
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     /* <> </> replacing Fragment */
     <>
-      <Cart />
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      {/* {cartIsShown && <Cart />} expression to display if both are true (Cart will always be true - it is a shortcut notation) */}
+
+      <Header onShowCart={showCartHandler} />
+      {/* props with functions conventionnaly have their name starts with on */}
+      
       <main>
         <Meals />
       </main>
