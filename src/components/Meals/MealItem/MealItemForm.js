fc2ffccc788313da560e4f4693c578137050be31
louisql <102247814+louisql@users.noreply.github.com>
2022-12-30 17:35:40 -0500

    Context implemented for Cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,47 @@
-import Input from '../../UI/Input';
+import { useRef, useState } from "react";
+/* Important: refs do not work by default on custom components. you have to go to the component,, import React there and wrap our component function with React Forwards rep */
+import Input from "../../UI/Input";
 
-import classes from './MealItemForm.module.css';
+import classes from "./MealItemForm.module.css";
 
-const MealItemForm = props =>{
-    return(
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount_' + props.id,
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
-            <button>+ Add</button>
-        </form>
-    )
-}
+const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
 
-export default MealItemForm
+    const submitHander = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;  /* Always use .current for refs created with useRef */
+        /* enteredAmount will be a string, using + to convert to a number */
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false);
+            return;
+        }
+
+        /* Here we just have amount and lack id, name, price so the context method will be called elsewhere */
+        props.onAddToCart(enteredAmountNumber);
+    }
+    
+  return (
+    <form className={classes.form} onSubmit={submitHander}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>¸
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+    </form>
+  );
+};
+
+export default MealItemForm;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from "react";
+
 import classes from  "./Input.module.css";
 
-const Input = props =>{
+const Input = React.forwardRef((props, ref) =>{
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id} >{props.label}</label>
-            <input {...props.input}/>
+            <input {...props.input}   ref={ref} />
             {/* {...props.input} is being used to add automatically props (all keys and their values)  */}
         </div>
     )
-}
+})
 
 export default Input
