ecf36aac345495734f8a12f3cfce7b131f5bd0da
louisql <102247814+louisql@users.noreply.github.com>
2022-12-30 23:51:06 -0500

    Items are removable from the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,63 @@
-import classes from './Cart.module.css';
-import Modal from "../UI/Modal"
+import { useContext } from "react";
 
-const Cart = props => {
-    const cartitems = <ul className={classes['cart-items']}>{
-        [{id:'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}
-    </ul> 
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 
-    return(
-        <Modal onClosePointer={props.onClose}>
-            {cartitems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>35.62</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
-            </div>
-        </Modal>
-    )
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id)
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1})
+  };
+
+  const cartitems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map(
+        (item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+            /* 
+            IMPORTANT
+              here we bind to preconfigure the argument the function will receive when it is executed 
+              because cartItemRemoveHandler is a reference to the function, not an execution (if there was no arguments we wouldn't have parentheses )
+             onRemove={() => cartItemRemoveHandler(item.id)} arrow notation to remove bind
+            */
+ 
+            onAdd={cartItemAddHandler.bind(null, item)} 
+          />
+        ) /* Do not forget to add key props when you have a list */
+      )}
+    </ul>
+  );
+
+  return (
+    <Modal onClosePointer={props.onClose}>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
 };
 
- export default Cart;
+export default Cart;
